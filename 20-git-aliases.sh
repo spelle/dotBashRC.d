77c1f042a1ab045755153b959f41aae79fc885d3
Sébastien PELLÉ <pelle.sebastien@gmail.com>
2014-11-26 20:38:11 +0100

    changed the way of working

---
+++ 20-git-aliases.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# GIT
+alias glg='git log --pretty=format:'\''%C(yellow)%h %Cred%ad %Cblue%an %Cgreen%d %Creset%s'\'' --oneline --abbrev-commit --all --graph --decorate --color'
+alias gct='git commit -m'
+alias gst='git status'
+alias gad='git add'
+alias gps='git push'
+alias gpl='git pull'
+
